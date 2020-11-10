798583cba7c83a2fe83db910d21670bc16d08904
Alexey Voznyuk <me@swizard.info>
2020-11-10 20:08:59 +0300

    add failing test and start planner bugfixing

--- src/writer/tests/plan.rs
+++ src/writer/tests/plan.rs
@@ -77,6 +77,61 @@ fn tree17_3() {
     ]);
 }
 
+#[test]
+fn tree22_3() {
+    let sketch = sketch::Tree::new(22, 3);
+
+    // Level { index: 0, blocks_count: 1, items_count: 3 }
+    // Level { index: 1, blocks_count: 3, items_count: 9 }
+    // Level { index: 2, blocks_count: 4, items_count: 10 }]
+
+    //                                   (12 17 21)
+    //                          (3 7 11)      (14 15 16) (18 19 20)
+    // (0 1 2) (4 5 6) (8 9 10)          (13)
+    interpret_script(&sketch, vec![
+        Instruction::TreeStart,
+        Instruction::BlockStart { level_index: 2, block_index: 0, items_count: 3, },
+        Instruction::WriteItem { level_index: 2, block_index: 0, item_index: 0 }, // 0
+        Instruction::WriteItem { level_index: 2, block_index: 0, item_index: 1 }, // 1
+        Instruction::WriteItem { level_index: 2, block_index: 0, item_index: 2 }, // 2
+        Instruction::BlockFinish { level_index: 2, block_index: 0 },
+        Instruction::BlockStart { level_index: 1, block_index: 0, items_count: 3, },
+        Instruction::WriteItem { level_index: 1, block_index: 0, item_index: 0 }, // 3
+        Instruction::BlockStart { level_index: 2, block_index: 1, items_count: 3, },
+        Instruction::WriteItem { level_index: 2, block_index: 1, item_index: 0 }, // 4
+        Instruction::WriteItem { level_index: 2, block_index: 1, item_index: 1 }, // 5
+        Instruction::WriteItem { level_index: 2, block_index: 1, item_index: 2 }, // 6
+        Instruction::BlockFinish { level_index: 2, block_index: 1 },
+        Instruction::WriteItem { level_index: 1, block_index: 0, item_index: 1 }, // 7
+        Instruction::BlockStart { level_index: 2, block_index: 2, items_count: 3, },
+        Instruction::WriteItem { level_index: 2, block_index: 2, item_index: 0 }, // 8
+        Instruction::WriteItem { level_index: 2, block_index: 2, item_index: 1 }, // 9
+        Instruction::WriteItem { level_index: 2, block_index: 2, item_index: 2 }, // 10
+        Instruction::BlockFinish { level_index: 2, block_index: 2 },
+        Instruction::WriteItem { level_index: 1, block_index: 0, item_index: 2 }, // 11
+        Instruction::BlockFinish { level_index: 1, block_index: 0 },
+        Instruction::BlockStart { level_index: 0, block_index: 0, items_count: 3, },
+        Instruction::WriteItem { level_index: 0, block_index: 0, item_index: 0 }, // 12
+        Instruction::BlockStart { level_index: 2, block_index: 0, items_count: 1, },
+        Instruction::WriteItem { level_index: 2, block_index: 0, item_index: 0 }, // 13
+        Instruction::BlockFinish { level_index: 2, block_index: 0 },
+        Instruction::BlockStart { level_index: 1, block_index: 1, items_count: 3, },
+        Instruction::WriteItem { level_index: 1, block_index: 1, item_index: 0 }, // 14
+        Instruction::WriteItem { level_index: 1, block_index: 1, item_index: 1 }, // 15
+        Instruction::WriteItem { level_index: 1, block_index: 1, item_index: 2 }, // 16
+        Instruction::BlockFinish { level_index: 1, block_index: 1 },
+        Instruction::WriteItem { level_index: 0, block_index: 0, item_index: 1 }, // 17
+        Instruction::BlockStart { level_index: 1, block_index: 2, items_count: 3, },
+        Instruction::WriteItem { level_index: 1, block_index: 2, item_index: 0 }, // 18
+        Instruction::WriteItem { level_index: 1, block_index: 2, item_index: 1 }, // 19
+        Instruction::WriteItem { level_index: 1, block_index: 2, item_index: 2 }, // 20
+        Instruction::BlockFinish { level_index: 1, block_index: 2 },
+        Instruction::WriteItem { level_index: 0, block_index: 0, item_index: 2 }, // 21
+        Instruction::BlockFinish { level_index: 0, block_index: 0 },
+        Instruction::Done,
+    ]);
+}
+
 #[derive(PartialEq, Debug)]
 enum Instruction {
     TreeStart,
